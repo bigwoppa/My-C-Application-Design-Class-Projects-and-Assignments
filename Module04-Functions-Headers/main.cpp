3d6bbfb61c2ac71be8c16f87819f2791efbc73ba
idris <salemtedjidris@gmail.com>
2026-09-21 14:43:03 -0700

    setting up the needed functions and init the data arrays to pull data from csv to place in.

--- Module04-Functions-Headers/main.cpp
+++ Module04-Functions-Headers/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 int main() {
-    string Majors[10] = {"Ada", "Sam", "Jordan"};
-    int MedianAnnual[10] = {91, 84, 97};
-    float UnemploymentRate[10] = {5, 7, 4};
+    string Majors[10];
+    int MedianAnnual[10];
+    float UnemploymentRate[10];
     
-    int *scorePtr = &MedianAnnual[0];
+    int *Pincome = &MedianAnnual[0];
 
     for (int i = 0; i < 3; i++) {
-        cout << Majors[i] << ": " << MedianAnnual[i] << endl;
+        cout << Majors[i] << ": " << MedianAnnual[i] << ", Unemployment Rate: " << UnemploymentRate[i] << "%" << endl;
     }
 
-    cout << "First score through pointer: " << *scorePtr << endl;
+    cout << "First income through pointer: " << *Pincome << endl;
     return 0;
 }
