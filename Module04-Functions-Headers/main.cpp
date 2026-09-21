bdac9bff2cbcd0693242178d9e1b2bfdab616073
idris <salemtedjidris@gmail.com>
2026-09-21 14:54:49 -0700

    made it more neat with iomanip, very useful for terminal pretty points. Basically done.

--- Module04-Functions-Headers/main.cpp
+++ Module04-Functions-Headers/main.cpp
@@ -2,18 +2,35 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-
+#include <iomanip>
 using namespace std;
+
+///Open the CSV file containing the data.
+
+
 
 int main() {
     string Majors[10];
-    int MedianAnnual[10];
+    string MedianAnnual[10];
     float UnemploymentRate[10];
-    
-    int *Pincome = &MedianAnnual[0];
+    ifstream dataFile("data.csv");
+    string line;
 
-    for (int i = 0; i < 3; i++) {
-        cout << Majors[i] << ": " << MedianAnnual[i] << ", Unemployment Rate: " << UnemploymentRate[i] << "%" << endl;
+    int count = 0;
+    while (count < 10 && getline(dataFile, line)) {
+        stringstream ss(line);
+        getline(ss, Majors[count], ',');
+        getline(ss, MedianAnnual[count], ',');
+        ss >> UnemploymentRate[count];
+        count++;
+    }
+    
+    string *Pincome = &MedianAnnual[0];
+
+    for (int i = 0; i < 10; i++) {
+        cout << left << setw(35) << Majors[i] << 
+        right << setw(12) << ": " << MedianAnnual[i] << 
+        setw(12) << ", Unemployment Rate: " << UnemploymentRate[i] << "%" << endl;
     }
 
     cout << "First income through pointer: " << *Pincome << endl;
